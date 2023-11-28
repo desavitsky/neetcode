842c99c666ffa431a47b3a2ac216d953d46736a6
denis_savitsky <d.e.savitskiy@gmail.com>
2023-11-28 20:26:17 +0100

    Trees part 1

---
+++ course1/src/desavitsky/trees/Task701.java
@@ -0,0 +1,13 @@
+package desavitsky.trees;
+
+
+
+public class Task701 {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        } else root.left = insertIntoBST(root.left, val);
+        return root;
+    }
+}
