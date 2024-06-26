283b18a02e15c34477e5b0549d5f57f75c1dfe6e
dsavitsky <dsavitsky@evolution.com>
2024-06-26 23:10:57 +0200

    Complete Trie

--- course2/src/desavitsky/trie/Task212.java
+++ course2/src/desavitsky/trie/Task212.java
@@ -1,58 +1,81 @@
 package desavitsky.trie;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Stream;
 
 // Word Search II
 public class Task212 {
 
     public static void main(String[] args) {
+        System.out.println(findWords(
+                new char[][]{{'o', 'a', 'a', 'n'}, {'e', 't', 'a', 'e'}, {'i', 'h', 'k', 'r'}, {'i', 'f', 'l', 'v'}},
+                new String[]{"oath", "pea", "eat", "rain"}
+        ));
 
+        System.out.println(findWords(
+                new char[][]{{'a', 'a'}},
+                new String[]{"aaa"}
+        ));
     }
 
-    public List<String> findWords(char[][] board, String[] words) {
+    public static List<String> findWords(char[][] board, String[] words) {
         var dict = new Dict();
         for (String word : words) {
             dict.insert(word);
         }
 
-        return findWordsHelper(board, dict, new Coordinate(0, 0), new HashSet<>(), "");
+        var result = new HashSet<String>();
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                var coord = new Coordinate(i, j);
+                var visited = new HashSet<Coordinate>();
+                visited.add(coord);
+                result.addAll(findWordsHelper(board, dict, dict.getRoot(), coord, visited, ""));
+            }
+        }
+
+        return result.stream().toList();
 
     }
 
     record Coordinate(int x, int y) {
         public List<Coordinate> next(int limitX, int limitY) {
             return Stream.of(
-                            new Coordinate(x + 1, y + 1),
-                            new Coordinate(x - 1, y + 1),
-                            new Coordinate(x - 1, y - 1),
-                            new Coordinate(x + 1, y - 1)
+                            new Coordinate(x, y + 1),
+                            new Coordinate(x, y - 1),
+                            new Coordinate(x - 1, y),
+                            new Coordinate(x + 1, y)
                     )
                     .filter(coord -> coord.x < limitX && coord.y < limitY && coord.x >= 0 && coord.y >= 0)
                     .toList();
         }
     }
 
-    private List<String> findWordsHelper(char[][] board, Dict dict, Coordinate coord, Set<Coordinate> visited, String word) {
+    private static List<String> findWordsHelper(char[][] board, Dict dict, Dict.DictNode node, Coordinate coord, Set<Coordinate> visited, String word) {
         var currentChar = board[coord.x][coord.y];
         var updWord = word + currentChar;
         var words = new ArrayList<String>();
-        if (dict.startsWith(updWord)) { // replace node, not boolean
-            if (dict.search(updWord)) {
+
+        var child = node.getChild(currentChar);
+
+        if (child != null) {
+            if (child.isWord()) {
+                System.out.println(STR."ADDING WORD: \{updWord}");
                 words.add(updWord);
+                dict.removeWord(updWord);
+                System.out.println(STR."IS WORD still there: \{dict.search(updWord)}");
             }
             coord.next(board.length, board[0].length)
                     .stream()
                     .filter(coord1 -> !visited.contains(coord1))
                     .forEach(coord1 -> {
                         visited.add(coord1);
-                        words.addAll(findWordsHelper(board, dict, coord1, visited, updWord));
+                        words.addAll(findWordsHelper(board, dict, child, coord1, visited, updWord));
                         visited.remove(coord1);
                     });
         }
+
         return words;
     }
 }
@@ -63,7 +86,6 @@ class Dict {
         private final DictNode[] children;
 
         private boolean isWord;
-
 
         public DictNode() {
             this.children = new DictNode[26];
@@ -77,6 +99,15 @@ class Dict {
         public DictNode getChild(char c) {
             return children[c - 'a'];
         }
+
+        public boolean isWord() {
+            return isWord;
+        }
+
+        public void deleteChild(char c) {
+            children[c - 'a'] = null;
+        }
+
     }
 
 
@@ -90,13 +121,11 @@ class Dict {
         var current = head;
         for (var c : word.toCharArray()) {
             var position = c - 'a';
-            if (current.children[position] != null) {
-                current = current.children[position];
-            } else {
-                var newNode = new DictNode();
-                current.children[position] = newNode;
-                current = newNode;
+            if (current.children[position] == null) {
+                current.children[position] = new DictNode();
             }
+            current = current.children[position];
+
         }
         current.setWord(true);
     }
@@ -110,13 +139,24 @@ class Dict {
         return current.isWord;
     }
 
-    public boolean startsWith(String prefix) {
-        var current = head;
-        for (var c : prefix.toCharArray()) {
-            current = current.children[c - 'a'];
-            if (current == null) return false;
+    public void removeWord(String word) {
+        removeWordHelper(word, 0, head);
+    }
+
+    private boolean removeWordHelper(String word, int index, DictNode node) {
+        if (index == word.length()) {
+            if (Arrays.stream(node.children).allMatch(Objects::isNull)) {
+                return true;
+            } else {
+                node.setWord(false);
+                return false;
+            }
+        } else {
+            if (removeWordHelper(word, index + 1, node.getChild(word.charAt(index)))) {
+                node.deleteChild(word.charAt(index));
+                return Arrays.stream(node.children).allMatch(Objects::isNull);
+            } else return false;
         }
-        return true;
     }
 
     public DictNode getRoot() {
---
+++ course2/src/desavitsky/trie/Task745.java
@@ -0,0 +1,79 @@
+package desavitsky.trie;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Task745 {
+    public static void main(String[] args) {
+        var f = new WordFilter(new String[]{"apple"});
+        System.out.println(f.f("a", "e"));
+    }
+}
+
+class WordFilter {
+
+    private TrieNode prefixTrie = new TrieNode();
+
+    public WordFilter(String[] words) {
+        for (var i = 0; i < words.length; i++) {
+            for (var comb : combinations(words[i])) {
+                prefixTrie.addWord(comb, i);
+            }
+        }
+    }
+
+    private List<String> combinations(String word) {
+        var str = word + "{" + word;
+        return IntStream.range(0, word.length() + 2) // 0 - 4
+                .boxed()
+                .map(str::substring)
+                .filter(s -> !s.startsWith("{"))
+                .toList();
+    }
+
+    public int f(String pref, String suff) {
+        return prefixTrie.getIndex(suff + "{" + pref);
+    }
+
+
+    class TrieNode {
+
+        private TrieNode[] children = new TrieNode[27];
+
+        private int index = 0;
+
+
+        public void addWord(String word, int index) {
+            var currentTrie = this;
+            for (var c : word.toCharArray()) {
+                var position = c - 'a';
+                if (currentTrie.children[position] == null) {
+                    currentTrie.children[position] = new TrieNode();
+                }
+                currentTrie = currentTrie.children[position];
+                currentTrie.setIndex(index);
+            }
+        }
+
+        public int getIndex(String part) {
+            var currentTrie = this;
+            for (var c : part.toCharArray()) {
+                currentTrie = currentTrie.children[c - 'a'];
+                if (currentTrie == null) return -1;
+            }
+            return currentTrie.getIndex();
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+    }
+}
